fa225af61bd51e543352a54194326e63ae76bf7b
Jasmeet Singh Arora <jasspn2014@gmail.com>
2021-02-23 18:01:42 +0530

    updated menu-item with withRouter() HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
+
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = () => (
+  <div>
+    <h1> Hats Page </h1>
+  </div>
+);
+
+const ErrorPage = () => (
+  <div>
+    <h1> Something went Wrong </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+        <Route component={ErrorPage} />
+      </Switch>
     </div>
   );
 }
