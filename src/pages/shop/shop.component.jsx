80ce13f95c77e3f5f7b2db2d42d24220bbfaad04
Jasmeet Singh Arora <jasspn2014@gmail.com>
2021-03-04 14:44:55 +0530

    created new reducers and created collections-overview component
    added collection-overview in shop component
    added a new reducer and selector for shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,10 @@
 import React from "react";
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview/collection-preview.component";
-class ShopPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      collections: SHOP_DATA,
-    };
-  }
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
 
-  render() {
-    const { collections } = this.state;
-    return (
-      <div>
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = () => (
+  <div>
+    <CollectionsOverview />
+  </div>
+);
 
 export default ShopPage;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,12 @@
 import { createStore } from "redux";
+import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 
+export const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
